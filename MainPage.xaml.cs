09de9a7b8d4a93188c197412b1bb54fb2e52e90f
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-11-16 01:41:04 -0800

    Work In Progress

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,83 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        // Declare a global variable list to hold a list of strings, names
+        List<string> names = new List<string>();
 
         public MainPage()
         {
             InitializeComponent();
+
+            // Add names to the list
+            names.Add("Desmond");
+            names.Add("Manjula");
+            names.Add("Kristyn");
+            names.Add("Josh");
+            names.Add("Will");
+
+            // Call the UpdateInfoEditor() to update the display with names
+            UpdateInfoEditor();
+            // Call UpdateCountLabel() to update the count of elements on the top label
+            UpdateCountLabel();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void UpdateInfoEditor()
         {
-            count++;
+            editorInfo.Text = ""; // Clear the current text
+            foreach (var name in names)
+            {
+                editorInfo.Text += name + Environment.NewLine; // Append each name with a newline
+            }
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void UpdateCountLabel()
+        {
+            // Refresh the labelCount with the current numbers of elements in the list
+            labelCount.Text = $"Count: {names.Count}";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void buttonAdd_Clicked(object sender, EventArgs e)
+        {
+            names.Add(entryName.Text); // Add the entered name to the list
+            UpdateInfoEditor(); // Update the editor to display the new list
+            UpdateCountLabel(); // Update the count label
+        }
+
+        private void buttonUpdate_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(entryIndex.Text, out int index) && index >= 0 && index < names.Count)
+            {
+                names[index] = entryName.Text; // Update the name at the specified index
+                UpdateInfoEditor(); // Update the editor to display the new list
+            }
+        }
+
+        private void buttonInsert_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(entryIndex.Text, out int index) && index >= 0 && index <= names.Count)
+            {
+                names.Insert(index, entryName.Text); // Insert the name at the specified index
+                UpdateInfoEditor(); // Update the editor to display the new list
+                UpdateCountLabel(); // Update the count label
+            }
+        }
+
+        private void buttonRemove_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(entryIndex.Text, out int index) && index >= 0 && index < names.Count)
+            {
+                names.RemoveAt(index); // Remove the name at the specified index
+                UpdateInfoEditor(); // Update the editor to display the new list
+                UpdateCountLabel(); // Update the count label
+            }
+        }
+
+        private void buttonClear_Clicked(object sender, EventArgs e)
+        {
+            names.Clear();
+            UpdateInfoEditor();
+            UpdateCountLabel();
         }
     }
 }
